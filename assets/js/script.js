68aea6e83f7a87c116a52201f5243caca6d2d77f
Ralph <143613948+ralphkb@users.noreply.github.com>
2024-06-06 16:05:45 +0300

    Improve code formatting with prettier and some other changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
-const toggleContainer = document.querySelector('.toggle-container');
-const toggleCircle = document.querySelector('.toggle-circle');
-const toggleIcon = document.getElementById('toggle-icon');
-
-// Check the initial mode from local storage (if available)
-const currentMode = localStorage.getItem('mode');
-if (currentMode === 'dark') {
+const toggleContainer = document.querySelector(".toggle-container");
+const toggleCircle = document.querySelector(".toggle-circle");
+const toggleIcon = document.getElementById("toggle-icon");
+const currentMode = localStorage.getItem("mode");
+if (currentMode === "dark") {
   enableDarkMode();
 }
 
-toggleContainer.addEventListener('click', function () {
-  if (document.body.classList.contains('dark-mode')) {
+toggleContainer.addEventListener("click", function () {
+  if (document.body.classList.contains("dark-mode")) {
     enableLightMode();
   } else {
     enableDarkMode();
@@ -17,92 +15,70 @@ toggleContainer.addEventListener('click', function () {
 });
 
 function enableDarkMode() {
-  // Move the circle to the right (dark mode)
-  toggleCircle.style.transform = 'translateX(100%)';
-
-  // Change the background color of the circle
-  toggleCircle.style.backgroundColor = '#000';
-
-  // Change the icon to the moon
-  toggleIcon.innerHTML = '<span>🌙</span>';
-
-  // Change the background color of the slider
-  toggleContainer.style.backgroundColor = '#00000063';
-
-  // Add the 'dark-mode' class to the body
-  document.body.classList.add('dark-mode');
-
-  // Store the current mode in local storage
-  localStorage.setItem('mode', 'dark');
+  toggleCircle.style.transform = "translateX(100%)";
+  toggleCircle.style.backgroundColor = "#000";
+  toggleIcon.innerHTML = "<span>🌙</span>";
+  toggleContainer.style.backgroundColor = "#00000063";
+  document.body.classList.add("dark-mode");
+  localStorage.setItem("mode", "dark");
 }
 
 function enableLightMode() {
-  // Move the circle to the left (light mode)
-  toggleCircle.style.transform = 'translateX(0)';
-
-  // Change the background color of the circle
-  toggleCircle.style.backgroundColor = '#fff';
-
-  // Change the icon to the sun
+  toggleCircle.style.transform = "translateX(0)";
+  toggleCircle.style.backgroundColor = "#fff";
   toggleIcon.innerHTML = '<span class="light-icon active">☀️</span>';
-
-  // Change the background color of the slider
-  toggleContainer.style.backgroundColor = '#fff8f8c9';
-
-  // Remove the 'dark-mode' class from the body
-  document.body.classList.remove('dark-mode');
-
-  // Store the current mode in local storage
-  localStorage.setItem('mode', 'light');
+  toggleContainer.style.backgroundColor = "#fff8f8c9";
+  document.body.classList.remove("dark-mode");
+  localStorage.setItem("mode", "light");
 }
-  
+
 const mobileToggle = document.getElementById("mobile-toggle");
 const mobileNav = document.getElementById("mobile-nav");
 
-  mobileToggle.addEventListener("click", function() {
-    mobileToggle.classList.toggle("active");
-    mobileNav.classList.toggle("active");
+mobileToggle.addEventListener("click", function () {
+  mobileToggle.classList.toggle("active");
+  mobileNav.classList.toggle("active");
 });
 
 // Function to simulate typing effect
 function typeEffect(element, text, delay) {
   let i = 0;
   const interval = setInterval(function () {
-      element.textContent += text.charAt(i);
-      i++;
-      if (i > text.length - 1) {
-          clearInterval(interval);
-          element.classList.add('hide-cursor');
-      }
+    element.textContent += text.charAt(i);
+    i++;
+    if (i > text.length - 1) {
+      clearInterval(interval);
+      element.classList.add("hide-cursor");
+    }
   }, delay);
 }
 
 // Call the typing effect function on page load
-window.addEventListener('load', function () {
-  const nameElement = document.getElementById('name');
-  const textToType = 'Ralph '; // Replace with your desired text
-  const delay = 200; // Adjust the delay (in milliseconds) to control typing speed
+window.addEventListener("load", function () {
+  const nameElement = document.getElementById("name");
+  const textToType = "Ralph ";
+  const delay = 200; // Delay of typing speed in milliseconds
 
-  typeEffect(nameElement, textToType, delay)
+  typeEffect(nameElement, textToType, delay);
 });
 
 // Add event listeners to all elements with the "tooltip" class
 const tooltips = document.querySelectorAll(".tooltip");
 
 tooltips.forEach((tooltip) => {
-    tooltip.addEventListener("mouseenter", () => {
-        const tooltipElement = tooltip.querySelector("::after");
-        if (tooltipElement) {
-            tooltipElement.style.opacity = "1";
-            tooltipElement.style.visibility = "visible";
-        }
-    });
+  tooltip.addEventListener("mouseenter", () => {
+    const tooltipElement = tooltip.querySelector("::after");
+    if (tooltipElement) {
+      tooltipElement.style.opacity = "1";
+      tooltipElement.style.visibility = "visible";
+    }
+  });
 
-    tooltip.addEventListener("mouseleave", () => {
-        const tooltipElement = tooltip.querySelector("::after");
-        if (tooltipElement) {
-            tooltipElement.style.opacity = "0";
-            tooltipElement.style.visibility = "hidden";
-        }
-    });
+  tooltip.addEventListener("mouseleave", () => {
+    const tooltipElement = tooltip.querySelector("::after");
+    if (tooltipElement) {
+      tooltipElement.style.opacity = "0";
+      tooltipElement.style.visibility = "hidden";
+    }
+  });
 });
